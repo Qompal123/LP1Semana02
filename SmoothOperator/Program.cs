c881b222947805e81db997ad5c29814c35fabd93
Zuha Rosario <zuhalourenco@gmail.com>
2025-02-26 17:52:15 +0000

    Add SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            
+            Console.Write("Insere número inteiro: ");
+
+            
+            string x = Console.ReadLine();
+
+            int y = int.Parse(x);
+
+            y--;
+
+            Console.WriteLine(y);
+
+            y++;
+
+            Console.WriteLine(y);
+
+
+        }
+    }
+}
